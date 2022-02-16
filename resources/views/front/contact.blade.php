ab3d94cc9d6cfbbd7d1fc34d1551d6931df933d7
AlbertWebs <albertmuhatia@gmail.com>
2022-02-16 20:33:52 +0300

    update contact page

--- resources/views/front/contact.blade.php
+++ resources/views/front/contact.blade.php
@@ -8,19 +8,19 @@
         </div>
       </header>
       <div class="article__map map" id="map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6303.983230561337!2d144.94987278893993!3d-37.813665379798834!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad65d4949e03ba7%3A0x3a4bed3cd2de0c6!2zMjY5IEtpbmcgU3QsIE1lbGJvdXJuZSBWSUMgMzAwMCwg0JDQstGB0YLRgNCw0LvQuNGP!5e0!3m2!1sru!2sru!4v1613041664241!5m2!1sru!2sru" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d31910.361004425693!2d36.86012042152098!3d-1.2976259198492563!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa0f9c38bd72cb408!2sDrenla%20Hub!5e0!3m2!1sen!2ske!4v1645032584477!5m2!1sen!2ske" allowfullscreen="true" aria-hidden="false" tabindex="0"></iframe>
         <div class="map__inner container">
           <div class="contact-block">
-            <div class="contact-block__heading">Melbourne, <span class="color-yellow">Australia</span>
+            <div class="contact-block__heading">Drenla Hub,<span class="color-yellow">Nairobi</span>
             </div>
-            <address class="contact-block__address">269 King Str, 05th Floor, Utral Hosue Building, Melbourne, VIC 3000, Australia.</address>
+            <address class="contact-block__address">Bamburi Rd, Off Enterprise Rd, Nairobi</address>
             <div class="contact-block__phone">
-              <a href="tel:+9903449564050">+99 (0) 344 956 4050</a>
+              <a href="tel:+254759947183">+254 075 994 7183</a>
             </div>
             <div class="contact-block__item">
               <div class="contact-block__item-hint">Email:</div>
               <div class="contact-block__item-val">
-                <a href="mailto:info@sparch.co">info@sparch.co</a>
+                <a href="mailto:info@sparch.co">info@drenla.com</a>
               </div>
             </div>
             <div class="contact-block__item">
@@ -29,7 +29,7 @@
                 <!-- Social-->
                 <ul class="contact-block__social social--contact social">
                   <li class="social__item">
-                    <a class="social__link" href="#" target="_blank">
+                    <a class="social__link" href="https://web.facebook.com/drenlahub" target="_blank">
                       <svg width="20" height="20" aria-label="facebook icon">
                         <use xlink:href="#facebook"></use>
                       </svg>
@@ -37,7 +37,7 @@
                     </a>
                   </li>
                   <li class="social__item">
-                    <a class="social__link" href="#" target="_blank">
+                    <a class="social__link" href="https://twitter.com/drenlahub" target="_blank">
                       <svg width="20" height="20" aria-label="twitter icon">
                         <use xlink:href="#twitter"></use>
                       </svg>
@@ -45,11 +45,11 @@
                     </a>
                   </li>
                   <li class="social__item">
-                    <a class="social__link" href="#" target="_blank">
-                      <svg width="20" height="20" aria-label="pinterest icon">
+                    <a class="social__link" href="https://www.instagram.com/drenlahub/" target="_blank">
+                      <svg width="20" height="20" aria-label="instagram icon">
                         <use xlink:href="#pinterest"></use>
                       </svg>
-                      <span class="visually-hidden">pinterest</span>
+                      <span class="visually-hidden">Instagram</span>
                     </a>
                   </li>
                 </ul>
@@ -67,7 +67,8 @@
           <h2 class="feedback__heading heading" data-aos="fade">Let's grab a coffee and jump on
             <br>conversation <span class="color-yellow">chat with us.</span>
           </h2>
-          <form class="js-form-validate" action="https://ultimatewebsolutions.net/larson/php/mail.php" method="POST">
+          <form class="js-form-validate" action="#" method="POST">
+              @csrf
             <div class="row">
               <div class="feedback__field-wrapper col-12 col-md-6 col-lg-4" data-aos="fade">
                 <label class="field" aria-label="Name">
